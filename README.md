e678cd1d4ad51967ca2f90e017282614217dd14e
Muhannad <Muhannadalmughrabi233@gmail.com>
2021-03-23 18:13:20 +0200

    finishes lab03

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 Viewing images on the page so that I can browse the photo collection from the json data, filtering the images so that I can view only images that match a keyword, also a simple, clean looking UI so that the photo gallery clearly displays the images in a grid like pattern but using floats. 
 
+# lab 02
 Number and name of feature: feature 1
 
 Estimate of time needed to complete:30mins
@@ -35,3 +36,48 @@ Start time: 7
 Finish time: 8
 
 Actual time needed to complete: 1hr
+
+# lab 03
+Number and name of feature: feature 1
+
+Estimate of time needed to complete:1hr
+
+Start time: 2:30
+
+Finish time: 3:30
+
+Actual time needed to complete: 1hr
+_________________________________
+
+Number and name of feature: feature 2
+
+Estimate of time needed to complete:1hr
+
+Start time: 3:40
+
+Finish time: 4:40
+
+Actual time needed to complete: 1hr
+
+______________________________
+
+Number and name of feature: feature 3
+
+Estimate of time needed to complete:1hr
+
+Start time: 4:50
+
+Finish time: 5:50
+
+Actual time needed to complete: 1hr
+
+________________________________
+Number and name of feature: feature 4
+
+Estimate of time needed to complete:30 mins
+
+Start time: 6:00
+
+Finish time: 6:15
+
+Actual time needed to complete: 15mins
--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 'use strict'
 let keywords = [];
 let renderArr = [];
+let id = $('#temp');
+let tester1 = true;
+let tester2;
+
 
 
 
@@ -14,29 +18,13 @@ function Items(item) {
     keywords.push(this.keyword);
     renderArr.push(this);
 
-
-
 }
 
 
 
 Items.prototype.render = function() {
-    // let photo_template = $('#photo-template').clone();
-    // $('main').append(photo_template);
-    // photo_template.find('h2').text(this.title);
-    // photo_template.find('img').attr('src', this.image);
-    // photo_template.find('p').text(this.description);
-    // photo_template.remove('#photo-template');
-    // photo_template.attr('id', this.keyword);
-    // console.log(this.keyword);
-    let temp = `<div id="${this.keyword}">
-    <h2>${this.title}</h2>
-    <img src="${this.image}" alt="${this.title}">
-    <p>${this.description}</p>
-  </div>`
-
-    $('main').append(temp)
-
+    let template = $(id).html();
+    $("main").append(Mustache.render(template, this));
 }
 
 
@@ -47,17 +35,30 @@ const ajaxSettings = {
 
 }
 
-$.ajax('data/page-1.json', ajaxSettings)
-    .then(data => {
-        data.forEach(item => {
-            let newItem = new Items(item);
-            // console.log(newItem);
-            newItem.render();
-        })
-        removeDuplicates();
-        appendKeywords();
+function showPage1() {
+    tester1 = true;
+    tester2 = false;
+    keywords = [];
+    uniquekeyWords = [];
+    renderArr = [];
+    $('main').empty();
+    $.ajax('data/page-1.json', ajaxSettings)
+        .then(data => {
+            data.forEach(item => {
+                let newItemPage1 = new Items(item);
+                // console.log(newItem);
+                newItemPage1.render();
+            })
+            removeDuplicates();
+            appendKeywords();
 
-    })
+        })
+
+}
+
+
+$('document').ready(showPage1);
+
 
 let uniquekeyWords = [];
 
@@ -70,9 +71,12 @@ function removeDuplicates() {
 }
 
 function appendKeywords() {
+    $('select').empty();
+    $('select').append(`<option value="default">Filter by Keyword </option>`);
     for (let i = 0; i < uniquekeyWords.length; i++) {
         let keyValue = uniquekeyWords[i];
         $('select').append(`<option>${keyValue}</option>`);
+        console.log(keyValue)
     }
 
 }
@@ -83,8 +87,145 @@ let filtering = (event) => {
     renderArr.forEach(elem => {
         if (elem.keyword == selectKeyWord) {
             elem.render();
+            console.log(renderArr);
         }
     })
 };
 
 $('select').on('change', filtering);
+
+function showPage2() {
+    tester2 = true;
+    tester1 = false;
+    keywords = [];
+    uniquekeyWords = [];
+    renderArr = [];
+    $('main').empty();
+    $.ajax('data/page-2.json', ajaxSettings).
+    then(data => {
+        data.forEach(item => {
+            // console.log(item)
+            let newItemPage2 = new Items(item);
+            // console.log(newItemPage2)
+            newItemPage2.render();
+        });
+        removeDuplicates();
+        appendKeywords();
+        $('select').on('change', filtering);
+
+    });
+}
+
+$('#secondpage').on('click', showPage2);
+$('#firstpage').on('click', showPage1);
+
+function sortHorns(data) {
+    data.sort((a, b) => {
+        console.log(a.horns - b.horns)
+        return a.horns - b.horns;
+
+    });
+    // console.log(data);
+};
+
+function sortTitle(data) {
+    data.sort((a, b) => {
+        return (a.title > b.title) ? 1 : -1;
+    })
+};
+
+function showHorns() {
+    $('main').empty();
+    if (tester1) {
+        $.ajax('data/page-1.json', ajaxSettings)
+            .then(data => {
+                sortHorns(data);
+                console.log(data);
+                data.forEach(item => {
+                    let neWsortedhorns = new Items(item);
+                    neWsortedhorns.render();
+                });
+            })
+    }
+    if (tester2) {
+        $.ajax('data/page-2.json', ajaxSettings)
+            .then(data => {
+                sortHorns(data);
+                data.forEach(item => {
+                    let neWsortedHorns = new Items(item)
+                    neWsortedHorns.render();
+                });
+
+            })
+    }
+    removeDuplicates();
+    appendKeywords();
+}
+
+
+
+function showTitle() {
+    $('main').empty();
+    if (tester1) {
+        $.ajax('data/page-1.json', ajaxSettings)
+            .then(data => {
+                sortTitle(data);
+                console.log(data);
+                data.forEach(item => {
+                    let neWsortedTitle = new Items(item);
+                    neWsortedTitle.render();
+                });
+            })
+    }
+    if (tester2) {
+        $.ajax('data/page-2.json', ajaxSettings)
+            .then(data => {
+                sortTitle(data);
+                data.forEach(item => {
+                    let neWsortedTitle = new Items(item)
+                    neWsortedTitle.render();
+                });
+
+            })
+    }
+    removeDuplicates();
+    appendKeywords();
+}
+
+64
+
+function checkingHorns() {
+    if ($('#horns').is(':checked')) {
+        showHorns();
+    } else {
+        if (tester1) {
+            showPage1();
+        }
+        if (tester2) {
+            showPage2();
+        }
+    }
+}
+
+function checkingTitle() {
+    if ($('#title').is(':checked')) {
+        showTitle();
+    } else {
+        if (tester1) {
+            showPage1();
+        }
+        if (tester2) {
+            showPage2();
+        }
+    }
+}
+
+
+// this $('#checkboxId').is(':checked') for verify if is checked
+
+// & this $("#checkboxId").prop('checked', true) to check
+
+// & this $("#checkboxId").prop('checked', false) to uncheck
+
+$('#horns').on('change', checkingHorns);
+$('#title').on('click', checkingTitle);
